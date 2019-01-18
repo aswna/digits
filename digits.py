d98a2e5321cddd5cd50f06cf119a367ada2c699a
aswna <aswna@server.fake>
2019-01-18 09:31:34 +0100

    Reboot for 2019

--- digits.py
+++ digits.py
@@ -1,99 +1,74 @@
 #!/usr/bin/env python3
 
-# TODO: see:
+# This is going to be the Python implementation of
 # https://mmlind.github.io/Simple_1-Layer_Neural_Network_for_MNIST_Handwriting_Recognition/
 
-
-import math
 import random
+from collections import namedtuple
 
-import test_data
-import training_data
+MNIST_Image = namedtuple(
+    "MNIST_Image", [
+        "pixel",  # 28x28
+    ]
+)
 
-DIM = len(training_data.TRAINING_DATA[0].data)
+MNIST_Label = namedtuple(
+    "MNIST_Label", [
+        "value",
+    ]
+)
 
+MNIST_ImageFileHeader = namedtuple(
+    "MNIST_ImageFileHeader", [
+        "magicNumber",
+        "maxImages",
+        "imgWidth",
+        "imgHeight",
+    ]
+)
 
-def main():
-    random.seed(12345)
-    digits_to_weights = {digit: train(digit) for digit in range(10)}
-    for digit, (weights, b) in digits_to_weights.items():
-        show_training_results(digit, weights, b)
-    test(digits_to_weights)
+MNIST_LabelFileHeader = namedtuple(
+    "MNIST_LabelFileHeader", [
+        "magicNumber",
+        "maxLabels",
+    ]
+)
 
+Cell = namedtuple(
+    "Cell", [
+        "input",  # 28x28
+        "weight",  # 28x28
+        "output",
+    ]
+)
 
-def train(digit):
-    print('TRAINING DATA for digit = {}'.format(digit))
-    range_limit_low = 0
-    range_limit_hi = 1
-    best_weights = [random.uniform(-range_limit_low, range_limit_hi)
-                    for i in range(DIM)]
-    best_b = random.uniform(-range_limit_low, range_limit_hi)
+Layer = namedtuple(
+    "Layer", [
+        "cell",  # cells (10)
+    ]
+)
 
-    min_sum_error_squared = 9999999999.9
-    for iteration in range(100000):
-        weights = [random.uniform(-range_limit_low, range_limit_hi)
-                   for i in range(DIM)]
-        b = random.uniform(-range_limit_low, range_limit_hi)
-
-        sum_error_squared = 0.0
-        for data in [
-                x for x in training_data.TRAINING_DATA if x.digit == digit]:
-            guessed_digit = guess_digit(data.data, weights, b)
-            error_squared = (guessed_digit - data.digit) ** 2
-            sum_error_squared += error_squared
-
-        if sum_error_squared < min_sum_error_squared:
-            min_sum_error_squared = sum_error_squared
-            best_weights = weights.copy()
-            best_b = b
-            # print(
-            #     'Found better parameters ({}): weights = {}, b = {} [{}]'
-            #     .format(iteration, weights, b, min_sum_error_squared))
-
-    print('Using best parameters: weights = {}, b = {} for digit = {}'
-          .format(best_weights, best_b, digit))
-    return (best_weights, best_b)
-
-
-def show_training_results(digit, weights, b):
-    print('SHOWING TRAINING DATA for DIGIT = {}'.format(digit))
-    for data in training_data.TRAINING_DATA:
-        guessed_digit = int(guess_digit(data.data, weights, b))
-        print('Actual digit = {} vs. guessed digit = {}'
-              .format(data.digit, guessed_digit))
+Vector = namedtuple(
+    "Vector", [
+        "val",  # vals (10)
+    ]
+)
 
 
-def test(digits_to_weights):
-    print('SHOWING TEST DATA')
-    for data in test_data.TEST_DATA:
-        print('Working on digit = {}'.format(data.digit))
-        guessed_digit = fit_digit(data.data, digits_to_weights)
-        print('Actual digit = {} vs. guessed digit = {}'
-              .format(data.digit, guessed_digit))
+def get_init_input():
+    return [0 for x in range(28*28)]
 
 
-def guess_digit(data, weights, b):
-    # return sigmoid(sum(weights[i] * data[i] for i in range(DIM)) + b) * 9.0
-    return sigmoid(sum(weights[i] * data[i] for i in range(DIM))) * 9.0
+def get_init_weight():
+    return [random.uniform(0, 1) for x in range(28*28)]
 
 
-def fit_digit(data, digits_to_weights):
-    min_error_squared = 9999999999.9
-    best_fit_digit = -1
-    for digit, (weights, b) in digits_to_weights.items():
-        guessed_digit = guess_digit(data, weights, b)
-        error_squared = (guessed_digit - digit) ** 2
-        print('  guess_digit = {}, error_squared = {}'.format(
-            guessed_digit, error_squared))
-        if error_squared < min_error_squared:
-            min_error_squared = error_squared
-            best_fit_digit = digit
-    return best_fit_digit
+def get_init_cell():
+    return Cell(get_init_input(), get_init_weight(), 0)
 
 
-def sigmoid(x):
-    return 1 / (1 + math.exp(-x))
+def initLayer():  # get_init_layer
+    cells = [get_init_cell() for x in range(10)]
+    return Layer(cells)
 
-
-if __name__ == "__main__":
-    main()
+# See file formats at http://yann.lecun.com/exdb/mnist/
