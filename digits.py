99d50a7b50014ce17683f90fc5593dcada738bb7
aswna <aswna@server.fake>
2019-01-25 09:27:36 +0100

    Start to read MNIST data file

--- digits.py
+++ digits.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
 
-# This is going to be the Python implementation of
-# https://mmlind.github.io/Simple_1-Layer_Neural_Network_for_MNIST_Handwriting_Recognition/
+"""This is going to be the Python implementation of
+https://mmlind.github.io/Simple_1-Layer_Neural_Network_for_MNIST_Handwriting_Recognition/
 
+Note: download and extract data files from http://yann.lecun.com/exdb/mnist/
+  train-images-idx3-ubyte.gz:  training set images (9912422 bytes)
+  train-labels-idx1-ubyte.gz:  training set labels (28881 bytes)
+  t10k-images-idx3-ubyte.gz:   test set images (1648877 bytes)
+  t10k-labels-idx1-ubyte.gz:   test set labels (4542 bytes)
+"""
+
+import struct
 import random
 from collections import namedtuple
+
 
 MNIST_Image = namedtuple(
     "MNIST_Image", [
@@ -55,11 +64,44 @@ Vector = namedtuple(
 )
 
 
+def main():
+    read_training_file()
+
+
+def read_training_file():
+    """ See file formats at http://yann.lecun.com/exdb/mnist/ """
+    filename = "./train-images-idx3-ubyte"
+    with open(filename, "rb") as f:
+        data = f.read()
+        if not data:
+            print("No data!")
+            exit(1)
+
+    struct_fmt = '>4i'  # int[4]
+    struct_len = struct.calcsize(struct_fmt)
+    buffer = data[:struct_len]
+    s = struct.unpack(struct_fmt, buffer)
+    print("%u %d %d %d" % s)
+    header = MNIST_ImageFileHeader(*s)
+
+    buffer = data[struct_len:]
+    struct_fmt = '>{}B'.format(header.imgHeight * header.imgWidth)
+    struct_len = struct.calcsize(struct_fmt)
+    images = [MNIST_Image(x) for x in struct.iter_unpack(struct_fmt, buffer)]
+    print("{}".format(images[0]))
+    for i, pixel in enumerate(images[0].pixel):
+        print("%3d " % pixel, end='')
+        if i % header.imgWidth == 0:
+            print("")
+
+
 def get_init_input():
+    # TODO: use header.imgHeight * header.imgWidth
     return [0 for x in range(28*28)]
 
 
 def get_init_weight():
+    # TODO: use header.imgHeight * header.imgWidth
     return [random.uniform(0, 1) for x in range(28*28)]
 
 
@@ -71,4 +113,6 @@ def initLayer():  # get_init_layer
     cells = [get_init_cell() for x in range(10)]
     return Layer(cells)
 
-# See file formats at http://yann.lecun.com/exdb/mnist/
+
+if __name__ == "__main__":
+    main()
